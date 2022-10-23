08b7e1e992f9acdb11b22cc6f284a5840dcd90f1
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2022-10-23 11:44:29 +0200

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        App::setLocale('en');
+
+        if (!session()->has('lang')) {
+            session()->put('en');
+        }
 
     }
 }
